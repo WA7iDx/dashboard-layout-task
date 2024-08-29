4d2729c5971bfc1d38ab4bcd67051618324e6d6d
WA7iDx <149006614+WA7iDx@users.noreply.github.com>
2024-08-29 13:09:51 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const menuBtn = document.querySelector(".menu-icon");
 const closeBtn = document.querySelector(".close-icon");
+const overlayMenu = document.querySelector(".overlay-screen");
 
 const overlayModeBtn = document.querySelector("#one");
 const visableModeBtn = document.querySelector("#two");
 
-const chatBtn = document.querySelector(".chat-icon");
+const chatBtn = document.querySelector(".chat-icon-img");
 const chatBox = document.querySelector(".allChat-box");
 
 overlayModeBtn.addEventListener("click", (e) => {
@@ -31,7 +32,21 @@ closeBtn.addEventListener("click", (e) => {
     document.body.classList.remove("open");
   }
 });
+overlayMenu.addEventListener("click", (e) => {
+  if (document.body.classList.contains("overlay-mode")) {
+    document.body.classList.remove("open");
+  }
+});
 
 chatBtn.addEventListener("click", (e) => {
   chatBox.classList.toggle("show");
+});
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("chat-icon-img")) {
+    return;
+  }
+
+  if (chatBox.classList.contains("show")) {
+    chatBox.classList.remove("show");
+  }
 });
